4e09bdd8cb23e5b36b503df7dee307654b0033f2
JmmonJeremy <suc19002@byui.edu>
2023-04-18 13:28:30 -0700

    Added spaces between code segments for readability

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,12 +6,15 @@ class Program
     {
         Console.WriteLine("Hello Prep1 Student Participant!");
         Console.WriteLine("");
+
         Console.Write("What is your first name? ");
-        string f_name;
+        string f_name;        
         f_name = Console.ReadLine();
+
         Console.Write("What is your last name? ");
         string l_name = Console.ReadLine();
         Console.WriteLine("");
+        
         Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
     }
 }
