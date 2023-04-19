cbd28d82d6a1bacc4cb4ed2c6dfa9b541bc67ec7
JmmonJeremy <suc19002@byui.edu>
2023-04-19 16:27:58 -0700

    Asked user for their grade percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,5 +7,9 @@ class Program
         // Greet the user, who is hopefully here to learn
         Console.WriteLine("Welcome to the Prep2 demonstration C# learners!");
         Console.WriteLine("");
+        
+        // Get the users grade % for input & store it
+        Console.Write("What is your grade percentage?");
+        string answer = Console.ReadLine();       
     }    
 }
