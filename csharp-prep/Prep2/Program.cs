1da66420208544035ebdc89302337f7efd915965
JmmonJeremy <suc19002@byui.edu>
2023-04-19 16:25:44 -0700

    Changed greeting message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        // Greet the user, who is hopefully here to learn
+        Console.WriteLine("Welcome to the Prep2 demonstration C# learners!");
+        Console.WriteLine("");
+    }    
 }
