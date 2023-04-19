9ca6069117d5eeb5830ae127b48619a3ad10e874
JmmonJeremy <suc19002@byui.edu>
2023-04-19 16:29:20 -0700

    Set up code to determine the letter grade from the percentage given.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,6 +10,32 @@ class Program
         
         // Get the users grade % for input & store it
         Console.Write("What is your grade percentage?");
-        string answer = Console.ReadLine();       
+        string answer = Console.ReadLine();
+         // Convert the string into a float
+        float gradePercentage = float.Parse(answer);
+        // Create a variable for the Letter Grade
+        string grade;
+
+        // Determine grade from the percentage
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }       
     }    
 }
