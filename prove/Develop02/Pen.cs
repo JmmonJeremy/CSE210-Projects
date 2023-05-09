5313febe4e39404b789ff121ffc242e546f24a9c
JmmonJeremy <suc19002@byui.edu>
2023-05-09 00:40:00 -0700

    Added using Collections to Menu class & created Pen class and its skeleton framework.

---
+++ prove/Develop02/Pen.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+// class to record temporary journal entries
+public class PromptUserName
+{
+  // variable to hold the date
+  string _date;
+  // variable to hold the used prompt
+  string _usedPrompt;
+  // variable to hold the entry
+  string _entry;
+  // variable to hold the entry number
+  string _entryNumber;
+  // list to hold the pending journal entries
+  List<string> _pendingEntries = new List<string>();
+
+  // method to add a temporary entry
+  public void Add()
+  {
+
+  }
+
+  // method to view pending journal entries
+  public void View()
+  {
+
+  }
+
+  // method to edit pending journal entries
+  public void Edit()
+  {
+    
+  }
+}
