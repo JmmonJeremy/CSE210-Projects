302006a219bf620b17ca0fc88fb921235ba1c8cd
JmmonJeremy <suc19002@byui.edu>
2023-06-16 13:31:26 -0700

    Added an _attributes variable to hold all the attributes in one string when loading them from a textfile. Added a _retrievedObjects list to hold all of the Goal objects in a list after they are retrieved from a textfile. Created SetRetrievedObjects, SetAttributes, GetAttributes, GetRetrievedOgjects, SetPoints, SetFilename, DivideAttributes virtual, LoadGoalList, methods and added a string parameter to the SetGoalTitle and SetDescription methods in the Goal class. All done in the process of making the 4th option on the Menu of 'Load your goals' fully functional.

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection;
 
 // ### CLASS ################################################ //
 // base class for goals
@@ -10,14 +11,18 @@ public class Goal
   private string _goalTitle;
   // variable to hold the goal's decription
   private string _description;
-  // variable to hold the value in points if the goal is completed
+  // variable to hold the value in points assigned to a goal
   private int _points;
   // variable to hold the value in earned points for the user
   private int _earnedPoints;
   // variable to hold a list of goals
   private List<Goal> _goalList = new List<Goal>();
   // varialbe to hold the filename for saving goals to a textfile
-  private string _filename;
+  private string _filename;  
+  // variable to hold the string of attributes retrieved from a textfile  
+  private string _attributes;
+  // list to hold the goal objects retrieved from a textfile
+  private List<Goal> _retrievedObjects = new List<Goal>();
 
 // ### CONSTRUCTORS ######################################### //
 // constructor to be able to use the Goal methods
@@ -45,6 +50,12 @@ public class Goal
   }
 
 // ### METHODS ############################################## //
+  // setter method for the _points variable
+  public void SetPoints(int points)
+  {
+    _points = points;
+  }
+
   // getter method for the _points variable
   public int GetPoints()
   {
@@ -81,15 +92,25 @@ public class Goal
   }
 
   // method to set the goal title
-  public void SetGoalTitle()
-  {      
-    // create user prompt for setting the goal title associated with this goal & save it in a variable
-    string goalTitlePrompt = "What is the title for your goal? ";
-    // create a validator object to run its method with
-    // and pass the prompt question into the object
-    Validator validator = new Validator(goalTitlePrompt);    
-    // set the _goalTitle equal to the string the ConfirmEntry method returns
-    _goalTitle = validator.ConfirmEntry(); 
+  public void SetGoalTitle(string goalTitle)
+  {  
+    // if user is setting it up pass in "userSets"   
+    if (goalTitle == "userSets")
+    {  
+      // create user prompt for setting the goal title associated with this goal & save it in a variable
+      string goalTitlePrompt = "What is the title for your goal? ";
+      // create a validator object to run its method with
+      // and pass the prompt question into the object
+      Validator validator = new Validator(goalTitlePrompt);    
+      // set the _goalTitle equal to the string the ConfirmEntry method returns
+      _goalTitle = validator.ConfirmEntry(); 
+    }
+    // otherwise
+    else
+    {
+      // set _goalTitle equal to what is passed in 
+      _goalTitle = goalTitle;
+    }
   }
 
   // method to get the goal title
@@ -99,15 +120,25 @@ public class Goal
   }
 
   // method to se the goal description
-  public void SetDescription()
-  {   
-    // create user prompt for setting the goal description associated with this goal & save it in a variable
-    string descriptionPrompt = "Please enter a short description of your goal: ";
-    // create a validator object to run its method with
-    // and pass the prompt question into the object
-    Validator validator = new Validator(descriptionPrompt);    
-    // set the _description equal to the string the ConfirmEntry method returns
-    _description = validator.ConfirmEntry(); 
+  public void SetDescription(string description)
+  { 
+    // if user is setting it up pass in "userSets" 
+    if (description == "userSets")
+    {  
+      // create user prompt for setting the goal description associated with this goal & save it in a variable
+      string descriptionPrompt = "Please enter a short description of your goal: ";
+      // create a validator object to run its method with
+      // and pass the prompt question into the object
+      Validator validator = new Validator(descriptionPrompt);    
+      // set the _description equal to the string the ConfirmEntry method returns
+      _description = validator.ConfirmEntry();
+    }
+    // otherwise
+    else
+    {
+      // set _description equal to what is passed in
+      _description = description;
+    } 
   }
 
   // method to get the goal description
@@ -125,10 +156,38 @@ public class Goal
     // Console.WriteLine($"The list count after adding the object is: {_goalList.Count}");
   }
 
-  // method to return the _goalList
+  // getter method for the _goalList
   public List<Goal> GetGoalList()
   {
     return _goalList;
+  }
+
+  // setter method for the _filename variable
+  public void SetFilename(string filenamePrompt)
+  {      
+    // create a validator object to run its method with
+    // and pass the prompt question into the object
+    Validator validator = new Validator(filenamePrompt);    
+    // set a variable equal to the string the ConfirmEntry method returns with .txt on the end      
+    _filename = $"{validator.ConfirmEntry()}.txt";
+  }
+  
+  // setter method for the _attributes variable
+  public void SetAttributes(string attributes)
+  {
+    _attributes = attributes;
+  }
+  
+  // getter method for the _attributes variable
+  public string GetAttributes()
+  {
+    return _attributes;
+  }
+
+  // getter method for the _retrievedObjects list
+  public List<Goal> GetRetrievedObjects()
+  {
+    return _retrievedObjects;
   }
 
   // method to list out a goal
@@ -192,7 +251,7 @@ public class Goal
   public virtual string CreateGoalText()
   {           
     // list the goal for the user to see
-    string goalText = $"{GetGoalType()}:{GetGoalTitle()}~|~{GetDescription()}~|~{GetPoints()}";
+    string goalText = $"{GetGoalType()}:~|~{GetGoalTitle()}~|~{GetDescription()}~|~{GetPoints()}";
     // return the listed goal string
     return goalText; 
   }
@@ -200,19 +259,14 @@ public class Goal
   // method to save goals to a text file
   public void SaveGoals()
   {
-    // ask the user for the filename to save the goals to & save the prompt to a variable
-    string filenamePrompt = "Please enter a filename to save the goals under: ";
-    // create a validator object to run its method with
-    // and pass the prompt question into the object
-    Validator validator = new Validator(filenamePrompt);    
-    // set the _filename equal to the string the ConfirmEntry method returns with .txt on the end
-    _filename = validator.ConfirmEntry() + ".txt";
+    // after the _filename is set load any previous Goal objects saved under this filename
+    LoadGoalList(); 
     // create a StreamWriter object to be able to write a textfile
     using (StreamWriter outputFile = new StreamWriter(_filename))
     {  
       // save this information to the textfile
-      // points earned
-      outputFile.WriteLine(_earnedPoints);
+      // Goal class and points earned
+      outputFile.WriteLine($"Goal:~|~{_earnedPoints}");
       // OneOffGoal's: goal class, goal title, description, point value, done: true or false
       // HabitGoal's : goal class, goal title, description, point value
       // AccrualGoal's: goal class, goal title, description, point value, bonus point value, accrual number, times done
@@ -224,4 +278,87 @@ public class Goal
       }    
     }
   }
+
+  // method to create an object of the class
+  public virtual Goal CreateObject()
+  {
+    // create an object
+    Goal goal = new Goal();
+    return goal;
+  }
+
+  // method to load a file of goal objects into a list
+  public void SetRetrievedOjects()
+  {    
+    // double check if file exists 1st
+    if (File.Exists(_filename))
+    {
+      // load all file entries to a string list    
+      string[] entries = System.IO.File.ReadAllLines(_filename);
+      // cycle through each entry
+      foreach (string entry in entries)
+      {
+        // seperate the string into the object and its attributes using the colon
+        string[] segments = entry.Split(":");  
+        // reference source: https://codereview.stackexchange.com/questions/4174/better-way-to-create-objects-from-strings & https://learn.microsoft.com/en-us/dotnet/api/system.reflection.assembly.getexecutingassembly?view=net-7.0 & https://medium.com/knowledge-pills/what-is-getexecutingassembly-in-c-1d7830f38f85# & https://learn.microsoft.com/en-us/dotnet/api/system.reflection.assembly.createinstance?view=net-7.0 
+        // #2 "as" Operator: https://www.geeksforgeeks.org/c-sharp-as-operator-keyword/
+        // ### ALTERNATE OPTION: create Goal object from the string of the Goal base class or Goal derived classes
+        // Goal goal = Assembly.GetExecutingAssembly().CreateInstance(segments[0]) as Goal;
+        // reference source: #1 Casing: https://exceptionnotfound.net/csharp-in-simple-terms-3-casting-conversion-parsing-is-as-and-typeof/ 
+        // #2 Activator.CreateInstance method: https://learn.microsoft.com/en-us/dotnet/api/System.Activator.Createinstance?view=net-7.0
+        // #3 Type.GetType method: https://stackoverflow.com/questions/4876683/c-sharp-convert-dynamic-string-to-existing-class & https://learn.microsoft.com/en-us/dotnet/api/system.type.gettype?view=net-7.0
+        // create a Goal object or instance from the string of the Goal base class or Goal derived classes
+        Goal goal = (Goal)Activator.CreateInstance(Type.GetType(segments[0]));
+        // store the attributes in the _attributes variable
+        goal.SetAttributes(segments[1]);
+        // load the object into the _retrievedOjects list
+        _retrievedObjects.Add(goal);             
+      }      
+    }
+    // if filename doesn't exist
+    else
+    {
+      // let the user know that this file doesn't exist
+      Console.Write("The filename ");
+      // change the color of the text to green so the text stands out
+      Console.ForegroundColor = ConsoleColor.Green;
+      // reference source: https://reactgo.com/csharp-remove-last-n-characters-string/
+      // show the user what they entered
+      Console.Write($"{_filename.Remove(_filename.Length-4)}");     
+      // reset the text color to the original settings
+      Console.ResetColor();
+      // let the user know that this file doesn't exist & to check their spelling
+      Console.WriteLine(" does not exist. Please check the spelling of your filename.");
+    }
+  }
+
+  // method to load _goalList with Goals from textfile
+  public void LoadGoalList()
+  {
+    // retrieve goals objects from textfile
+    SetRetrievedOjects();
+    // cycle through the _retrievedObjects list
+    foreach (Goal type in GetRetrievedObjects())
+    {
+      // fill the goal object attributes & put in __goalList
+      type.DivideAttributes();
+      // if the Goal object has a _goalTitle
+      if (!string.IsNullOrEmpty(type.GetGoalTitle()))
+      {
+        // add the goal Goal object to the _goalList
+        GetGoalList().Add(type);
+      }
+    }
+  }
+
+  // method to break up retrieved attribute into the different variables
+  public virtual void DivideAttributes()
+  {   
+    // reference source: https://www.c-sharpcorner.com/UploadFile/mahesh/split-string-in-C-Sharp/#
+    // split the attribute string by its "~|~" separator characters
+    string[] attributes = GetAttributes().Split("~|~");
+    // fill the _earnedPoints variable with the right hand side of the 1st split
+    _earnedPoints = int.Parse(attributes[1]);    
+  }
+  
 }
