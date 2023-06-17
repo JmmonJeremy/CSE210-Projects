199ac790c43a256362a5bb2586ef8429e7abfa48
JmmonJeremy <suc19002@byui.edu>
2023-06-16 20:43:14 -0700

    Removed the Goal parameter for the 2 methods that required it and added a _goal object variable to  use throughout all methods.

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -10,7 +10,9 @@ public class Menu
   // variable to hold the user's choice and to run the while loop
   private string _choice = "run program";
   // variable to hold the filename prompt
-  private string _filenamePrompt;  
+  private string _filenamePrompt; 
+  // variable to hold a Goal object to be used throughout all Menu methods
+  private Goal _goal = new Goal(); 
 
 // ### CONSTRUCTORS ######################################### //
   // no constructors needed
@@ -18,7 +20,7 @@ public class Menu
   // method to present the menu to the user and to return the user's choice
   // passing in an object to the object is not recreated after running a menu option
   // this also allow the method to present the user's earned points with the object
-  public string PresentMainMenu(Goal goal)
+  public string PresentMainMenu()
   {     
     // create a string to hold the user's selection 
     // and to keep the while loop running until there's a valid entry
@@ -27,7 +29,7 @@ public class Menu
     while (selection != "1" && selection != "2" && selection != "3" && selection != "4" && selection != "5" && selection != "6")
     {
       // show the user how many points they have
-      goal.DisplayPoints();
+      _goal.DisplayPoints();
       // display the menu and directions
       Console.WriteLine("Select an option by entering its number:");
       Console.WriteLine(" 1 - Create a new goal");
@@ -98,7 +100,7 @@ public class Menu
   }
 
   // method to run the user's choice for the goal type 
-  public void RunGoalTypeChoices(Goal goal)
+  public void RunGoalTypeChoices()
   {  
     // determine the type of goal to create
     string goalType = PresentGoalTypeMenu();
@@ -106,64 +108,62 @@ public class Menu
     if (goalType == "1")
     {
       // set the values for the _goalTitle & _description
-      goal.SetGoalTitle("userSets");
-      goal.SetDescription("userSets");
+      _goal.SetGoalTitle("userSets");
+      _goal.SetDescription("userSets");
       // create a OneOffGoal object
-      OneOffGoal oneOff = new OneOffGoal(goal.GetGoalTitle(), goal.GetDescription());
+      OneOffGoal oneOff = new OneOffGoal(_goal.GetGoalTitle(), _goal.GetDescription());
       // load the goal into the list
-      goal.SetGoalList(oneOff);                    
+      _goal.SetGoalList(oneOff);                    
     } 
     // if the goal type is a habit
     if (goalType == "2")
     {
       // set the values for the _goalTitle & _description
-      goal.SetGoalTitle("userSets");
-      goal.SetDescription("userSets");
+      _goal.SetGoalTitle("userSets");
+      _goal.SetDescription("userSets");
       // create a HabitGoal object
-      HabitGoal habit = new HabitGoal(goal.GetGoalTitle(), goal.GetDescription());
+      HabitGoal habit = new HabitGoal(_goal.GetGoalTitle(), _goal.GetDescription());
       // load the goal into the list
-      goal.SetGoalList(habit); 
+      _goal.SetGoalList(habit); 
     }
     // if the goal type is a accrual
     if (goalType == "3")
     {
       // set the values for the _goalTitle & _description
-      goal.SetGoalTitle("userSets");
-      goal.SetDescription("userSets");
+      _goal.SetGoalTitle("userSets");
+      _goal.SetDescription("userSets");
       // create an AccrualGoal object
-      AccrualGoal accrual = new AccrualGoal(goal.GetGoalTitle(), goal.GetDescription());
+      AccrualGoal accrual = new AccrualGoal(_goal.GetGoalTitle(), _goal.GetDescription());
       // set the values for the _accrualNumber & _bonusPoints
       accrual.SetAccrualNumber("userSets");
       accrual.SetBonusPoints("userSets");
       // load the goal into the list
-      goal.SetGoalList(accrual); 
+      _goal.SetGoalList(accrual); 
     }    
   }
 
   // method to run the user's choice from the main menu
   public void RunMainChoices()
   {
-    // create a Goal object to be used throughout running of this menu
-    Goal goal = new Goal();
+    // run this until the user chooses to quit
     while (_choice != "6")
-    { 
-     
+    {      
       // use the PresentMainMenu method to display menu options and return
       // the user's choice - then store it in the while loop variable
-      _choice = PresentMainMenu(goal);
+      _choice = PresentMainMenu();
       // run the options the user chose
       // if they chose a to create a new goal
       if (_choice == "1")
       {  
         // present the user with the goal type choices
-        RunGoalTypeChoices(goal);
+        RunGoalTypeChoices();
       }   
       
        // if they chose to list their goals
       if (_choice == "2")
       {        
         // display the user's list of goals to them
-        goal.ListGoals();        
+        _goal.ListGoals();        
       }
       // if they chose to save their goals
       if (_choice == "3")
@@ -171,9 +171,9 @@ public class Menu
         // set the _filenamePrompt to pass into the SetFileName method
         _filenamePrompt = "Please enter a filename to save the goals under: ";
         // set the _filename from the Goal class
-        goal.SetFilename(_filenamePrompt);
+        _goal.SetFilename(_filenamePrompt);
         // save the _earnedPoints and _goalList to a textfile
-        goal.SaveGoals();
+        _goal.SaveGoals();
       }
       // if they chose to load their goals
       if (_choice == "4")
@@ -181,9 +181,9 @@ public class Menu
         // set the _filenamePrompt to pass into the SetFileName method
         _filenamePrompt = "What is the filename for the goals you would like to load? ";       
         // set the _filename from the Goal class
-        goal.SetFilename(_filenamePrompt);
+        _goal.SetFilename(_filenamePrompt);
         // load the specified textfile as Goal objects into the _goalList
-        goal.LoadGoalList();            
+        _goal.LoadGoalList();            
       } 
       // if they chose to record their completion of a goal
       if (_choice == "5")
