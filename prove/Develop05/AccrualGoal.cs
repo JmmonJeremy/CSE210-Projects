622dbb56559db23a9ddc6bd82c4a7ca55d7422e2
JmmonJeremy <suc19002@byui.edu>
2023-06-18 00:32:00 -0700

    In the AccrualGoal class I added clearer comments for the first constructor.

--- prove/Develop05/AccrualGoal.cs
+++ prove/Develop05/AccrualGoal.cs
@@ -14,8 +14,8 @@ public class AccrualGoal : Goal
   private int _bonusPoints;
 
 // ### CONSTRUCTORS ######################################### //
-  // constructor to be able to use the AccrualGoal methods
-  // and to return used goals saved to a textfile
+  // constructor used to return goals saved to a textfile by
+  // creating an AccrualGoal object in the GetGoalType method
   public AccrualGoal()
   {    
     // nothing needed in here 
@@ -30,47 +30,45 @@ public class AccrualGoal : Goal
   }
 // ### METHODS ############################################## //
   // method to set the _accrualNumber variable
-  public void SetAccrualNumber(string accrualNumber)
-  {   
-    // if the user is setting the _accrualNumber
-    if (accrualNumber == "userSets")
-    { 
-      // create user prompt for setting the accrual number associated with this goal & save it in a variable
-      string accrualNumberPrompt = "Please enter the number of times the goal needs to be done to reach the goal: ";
-      // create a validator object to run its method with
-      // and pass the prompt question into the object
-      Validator validator = new Validator(accrualNumberPrompt);    
-      // set the _accrualNumber equal to the int number the StringNumberCheck method returns
-      _accrualNumber = validator.StringNumberCheck(); 
-    } 
-    // otherwise 
-    else
-    {
-      // set the _accrualNumber to the string passed in converted to an int
-      _accrualNumber = int.Parse(accrualNumber);
-    }  
+  public void SetAccrualNumber()
+  {        
+    // create user prompt for setting the accrual number associated with this goal & save it in a variable
+    string accrualNumberPrompt = "Please enter the number of times the goal needs to be done to reach the goal: ";
+    // create a validator object to run its method with
+    // and pass the prompt question into the object
+    Validator validator = new Validator(accrualNumberPrompt);    
+    // set the _accrualNumber equal to the int number the StringNumberCheck method returns
+    _accrualNumber = validator.StringNumberCheck();   
+  }
+
+  // method to get the _accrualNumber
+  public int GetAccrualNumber()
+  {
+    return _accrualNumber;
   }
 
   // method to set the _bonusPoints variable
-  public void SetBonusPoints(string bonusPoints)
-  {   
-    // if the user is setting the _bonurPoints
-    if (bonusPoints == "userSets")
-    {    
-      // create user prompt for setting the bonus points associated with this goal & save it in a variable
-      string bonusPointsPrompt = "What is the bonus point amount for reaching the goal: ";
-      // create a validator object to run its method with
-      // and pass the prompt question into the object
-      Validator validator = new Validator(bonusPointsPrompt);    
-      // set the _bonusPoints equal to the int number the StringNumberCheck method returns
-      _bonusPoints = validator.StringNumberCheck();
-    } 
-    // otherwise 
-    else
-    {
-      // set the _bonusPoints to the string passed in converted to an int
-      _bonusPoints = Convert.ToInt32(bonusPoints);
-    }     
+  public void SetBonusPoints()
+  {       
+    // create user prompt for setting the bonus points associated with this goal & save it in a variable
+    string bonusPointsPrompt = "What is the bonus point amount for reaching the goal: ";
+    // create a validator object to run its method with
+    // and pass the prompt question into the object
+    Validator validator = new Validator(bonusPointsPrompt);    
+    // set the _bonusPoints equal to the int number the StringNumberCheck method returns
+    _bonusPoints = validator.StringNumberCheck();       
+  }
+
+  // method to get the _bonusPoints
+  public int GetBonusPoints()
+  {
+    return _bonusPoints;
+  }
+
+  // method to get the _completedCount
+  public int GetCompletedCount()
+  {
+    return _completedCount;
   }
 
   // method to list out a goal
@@ -101,7 +99,7 @@ public class AccrualGoal : Goal
   public override string CreateGoalText()
   {           
     // list the goal for the user to see
-    string goalText = $"{GetGoalType()}:{GetCompletedBox()}~|~{GetGoalTitle()}~|~{GetDescription()}~|~{GetPoints()}~|~{_bonusPoints}~|~{_accrualNumber}~|~{_completedCount}~|~{GetGoalCompleted()}";
+    string goalText = $"{GetGoalType()}:{GetCompletedBox()}~|~{GetGoalTitle()}~|~{GetDescription()}~|~{GetPoints()}~|~{GetBonusPoints()}~|~{GetAccrualNumber()}~|~{GetCompletedCount()}~|~{GetGoalCompleted()}";
     // return the listed goal string
     return goalText; 
   }
@@ -123,13 +121,40 @@ public class AccrualGoal : Goal
     SetPoints(int.Parse(attributes[3]));
     // reference source: https://www.shekhali.com/understanding-the-difference-between-int-int16-int32-and-int64-in-c-sharp/# & https://www.freecodecamp.org/news/how-to-convert-a-string-to-an-integer-in-c-sharp/#
     // fill the _bonusPoints with the next string from the split converted to an int by the method     
-    SetBonusPoints(attributes[4]);        
+    _bonusPoints = Convert.ToInt32(attributes[4]);        
     // fill the _accrualNumber with the next string from the split converted to an int by the method
-    SetAccrualNumber(attributes[5]); 
+    _accrualNumber = int.Parse(attributes[5]); 
     // fill the _completedCount with the last string from the split converted to an int 
     _completedCount = int.Parse(attributes[6]);
     // reference source: https://stackoverflow.com/questions/49590754/convert-a-string-to-a-boolean-in-c-sharp
     // fill the _goalCompleted boolean with the last string from the split converted to a bool
     SetGoalCompleted(bool.Parse(attributes[7]));  
+  }
+
+  // method to make changes when recording goal completion
+  public override void MarkComplete()
+  {
+    // when the _completedCount is less than one under the _accrualNumber 
+    if (_completedCount < _accrualNumber -1)
+    {
+      // set the completedCount value to one greater
+      _completedCount += 1;
+      // don't mark the goal as completed in the _completedBox variable
+      SetCompletedBox("[ ]");
+      // keep the _goalCompleted bool to false
+      SetGoalCompleted(false); 
+    }
+    // otherwise 
+    else
+    {
+      // set the completedCount value to one greater
+      _completedCount += 1;
+      // mark the goal as completed in the _completedBox variable
+      SetCompletedBox("[X]");
+      // change the _goalCompleted bool to true
+      SetGoalCompleted(true);
+      // add the _bonusPoint value to the _point value, so it will be added to the _earnedPoints
+      SetPoints(GetPoints() + _bonusPoints);
+    }  
   }
 }
