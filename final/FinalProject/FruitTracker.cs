a78bfa2bdfdbf486fefcd97937b232adfad2e7fb
JmmonJeremy <suc19002@byui.edu>
2023-07-01 17:01:14 -0700

    Created a FruitTracker class that inherits from the Tracker class and has nothing unique at this point, but works to create a fruit object in a list, save it to file and recreate it.

---
+++ final/FinalProject/FruitTracker.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+// ### CLASS ################################################ //
+// class for tracking the amount of fruit eaten
+public class FruitTracker : Tracker
+{
+// ### VARIABLE ATTRIBUTES ################################## //  
+  // private List<Fruit> _fruits = new List<Fruit>();
+// ### CONSTRUCTORS ######################################### //  
+  public FruitTracker()
+  {
+    // nothing needed in here 
+  }
+// ### METHODS ############################################## //
+  // method to figure out the total for the tracked value
+  public override void TotalTrackedValue()
+  {
+    
+  }
+
+  // method to total
+}
