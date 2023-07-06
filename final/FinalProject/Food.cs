a070d1b0148340270cac50c88c9f75cbd5489b05
JmmonJeremy <suc19002@byui.edu>
2023-07-05 20:57:26 -0700

    Changed things so that Food contains all foods instead of having a seperate class for each food.

--- final/FinalProject/Food.cs
+++ final/FinalProject/Food.cs
@@ -4,31 +4,30 @@ using System.Reflection;
 
 // ### CLASS ################################################ //
 // base class for foods
-public class Food
+public class Food : Tracked
 {
 // ### VARIABLE ATTRIBUTES ################################## //
-  private string _foodType;   
+  // private string _foodType;   
   private string _foodName;  
-  private int _portion; 
-  private string _portionScale; 
-  private int _calories; 
-  private string _attributes; 
+  // private int _portion; 
+  // // private string _portionScale; 
+  // private int _calories;  
   
 // ### CONSTRUCTORS ######################################### //
   // main constructor to set up a Food object with the user's inputs used in Menu class
-  public Food(string foodType, string portionScale)
+  public Food(string category, string measurement) :base (category, measurement)
   { 
-    _foodType = foodType;
-    _portionScale = portionScale;
+    // _foodType = foodType;
+    // _portionScale = portionScale;
     // #1 USER SETS _foodName **************************************************
-    string foodNamePrompt = $"What is the name of the {_foodType}? ";    
+    string foodNamePrompt = $"What is the name of the {_category}? ";    
     // pass the foodNamePrompt into the object & for the user's 
     // entry value put "Use prompt" since user will change value after the prompt
     Validator validator = new Validator("Use prompt", foodNamePrompt);    
     // with "Use prompt" set the method to to use the prompt the first time the method is used
     _foodName = validator.ConfirmEntry("Use prompt");
     // #2 USER SETS _portion ***************************************************
-    string portionPrompt = $"How many {_portionScale} is the {_foodType} you are adding? ";    
+    string portionPrompt = $"How many {_measurement} is the {_category} you are adding? ";    
     // pass the portionPrompt into the object & for the user's 
     // entry value put "Use prompt" since user will change value after the prompt
     Validator validator1 = new Validator("Use prompt", portionPrompt);    
@@ -36,7 +35,7 @@ public class Food
     // also set to use the ConfirmEntry method after validating number with "Do ConfirmEntry"
     _portion = validator1.StringNumberCheck("Use prompt", "Do ConfirmEntry");
     // #3 USER SETS _calories ***************************************************
-    string caloriesPrompt = $"How many calories is the {_foodType} you are adding? ";    
+    string caloriesPrompt = $"How many calories is the {_category} you are adding? ";    
     // pass the caloriesPrompt into the object & for the user's 
     // entry value put "Use prompt" since user will change value after the prompt
     Validator validator2 = new Validator("Use prompt", caloriesPrompt);    
@@ -46,17 +45,10 @@ public class Food
   }  
 
   // constructor for converting textfile to Food object in Tracker Class & for the ClassToString method in derived classes
-  public Food(string stringAttributes)
+  public Food(string stringAttributes) :base (stringAttributes)
   {
-    if (stringAttributes == "Set up empty")
-    {      
-      // do nothing to have an empty object set up
-    }
-    else
-    {      
-      // divides single string of attributes from a textfile into assigned individual attributes
-      DivideAttributes(stringAttributes); 
-    }
+    // For textfile to Food object uses DivideAttributes(stringAttributes) method, which
+    // divides single string of attributes from a textfile into assigned individual attributes  
   }
 
 // ### METHODS ############################################## //  
@@ -68,21 +60,22 @@ public class Food
 
 // START OF GROUPING OF 2 METHODS THAT HELP CONVERT OBJECT TO A STRING USED IN TRACKER & DERIVED CLASSES
   // method to create & return a food text string
-  public virtual string CreateFoodString(Object type)
-  {         
-    string foodString = $"{type.GetType()}:{_foodName}~|~{_portion}~|~{_calories}";    
+  public override string CreateTrackedString(Object type)
+  {     
+    string foodString = base.CreateTrackedString(type);     
+    foodString += $"~|~{_foodName}";        
     return foodString; 
   }
 // END OF GROUPING OF 2 METHODS THAT HELP CONVERT OBJECT TO A STRING USED IN TRACKER & DERIVED CLASSES
 
 // START OF GROUPING OF 1 METHOD THAT CONVERTS TEXT STRING TO OBJECT ATTRIBUTES USED IN CONSTRUCTOR
   // method to divide the string attributes stirng into their object's variable attributes  
-  private void DivideAttributes(string stringAttributes)
-  {       
-    string[] attributes = stringAttributes.Split("~|~");    
-    _foodName = attributes[0];     
-    _portion = Convert.ToInt32(attributes[1]);
-    _calories = int.Parse(attributes[2]);
+  public override void DivideAttributes(string stringAttributes)
+  {   
+    // reference source: https://stackoverflow.com/questions/36911460/adding-to-virtual-function-in-derived-class
+    base.DivideAttributes(stringAttributes); 
+    string[] attributes = stringAttributes.Split("~|~");
+    _foodName = attributes[3];     
   }
 // END OF GROUPING OF 1 METHOD THAT CONVERTS TEXT STRING TO OBJECT ATTRIBUTES USED IN CONSTRUCTOR
 }
