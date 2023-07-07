433bd25d6e74bbf1404559e00f22392f8a851636
JmmonJeremy <suc19002@byui.edu>
2023-07-07 10:50:47 -0700

    Removed the methods that were previously overriding methods that were no longer needed.

--- final/FinalProject/FoodTracker.cs
+++ final/FinalProject/FoodTracker.cs
@@ -14,22 +14,6 @@ public class FoodTracker : Tracker
     // nothing needed in here 
   }
 // ### METHODS ############################################## //
-  // method to display the desirec item objects in the _items list
-  public override void DisplayObjects()
-  { 
-     foreach (Food item in _items)
-    {
-      Console.WriteLine(item.CreateTrackedString(item)); // cast item object as a Food to use its method
-    }    
-  }
-
-  // method to return the selected object from the list
-  public override Food ReturnObject(int indexNumber)
-  {
-    Food selection = (Food)_items[indexNumber];    
-    return selection;  
-  }
-
   // method to figure out the total for the tracked value
   public override void TotalTrackedValue()
   {
