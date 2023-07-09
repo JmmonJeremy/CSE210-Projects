cc3e36306095acaac6ecf9ce5672b58b0e74cacf
JmmonJeremy <suc19002@byui.edu>
2023-07-08 19:11:40 -0700

    Added empty shell methods and changed name to cover both Recipe and Meal since their behavior and make up is so similar.

---
+++ final/FinalProject/FoodComboTracker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+// ### CLASS ################################################ //
+// class for tracking meals and their calories and portions
+public class MealTracker : Tracker
+{
+// ### VARIABLE ATTRIBUTES ################################## //   
+  private string _mealSet; // variable to hold the set being tracked
+
+// ### CONSTRUCTORS ######################################### //  
+  public MealTracker()
+  {
+    // nothing needed in here 
+  }
+
+// ### METHODS ############################################## //
+  // method to figure out the total for the tracked value
+  public override float TotalTrackedValue()
+  {
+    return base.TotalTrackedValue();
+  }
+
+  public override void DisplayObjects()
+  {
+    base.DisplayObjects();
+  }
+
+  public override void RemoveObject()
+  {
+    base.RemoveObject();
+  }
+}
